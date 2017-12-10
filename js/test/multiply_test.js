f606ea2f94c3ba7a8886625d041636187d982272
Heitor Silva <heitor-silva@hotmail.com>
2017-12-10 16:56:58 -0200

    Add eslint to JS solutions

--- js/test/multiply_test.js
+++ js/test/multiply_test.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const fn = require('../multiply.js');
 
-describe("Multiply", () => {
-  it("should return the product of a multiplication", () => {
+describe('Multiply', () => {
+  it('should return the product of a multiplication', () => {
     assert.equal(fn.multiply(2, 2), 4);
   });
 });
