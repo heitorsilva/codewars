ccdf41b365357c66f4674fea70c698111ec4c539
Heitor Silva <heitor-silva@hotmail.com>
2018-03-18 01:10:25 -0300

    Fix ruby lint errors

--- ruby/spec/multiply_spec.rb
+++ ruby/spec/multiply_spec.rb
@@ -5,10 +5,9 @@ require_relative '../multiply'
 
 describe 'Multiply' do
   context 'must receive A and B params' do
-     it 'should return their product' do
-       product = multiply(2, 3)
-       expect(product).to eq(6)
-     end
+    it 'should return their product' do
+      product = multiply(2, 3)
+      expect(product).to eq(6)
+    end
   end
 end
-
