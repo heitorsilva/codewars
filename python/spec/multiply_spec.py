7271726b7b0c65b63a2122bb3edc326e740bf900
Heitor Silva <heitor-silva@hotmail.com>
2018-05-28 00:18:15 -0300

    Fix Python lint errors

--- python/spec/multiply_spec.py
+++ python/spec/multiply_spec.py
@@ -4,7 +4,7 @@ from expects import expect, equal
 from multiply import multiply
 
 with description('Multiply') as self:
-  with context('must receive A and B params'):
-    with it('should return their product'):
-      product = multiply(2, 3)
-      expect(product).to(equal(6))
+    with context('must receive A and B params'):
+        with it('should return their product'):
+            product = multiply(2, 3)
+            expect(product).to(equal(6))
