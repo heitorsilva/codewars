80d0a830379685c334ac1dd4dd35385936fa7e74
Heitor Silva <heitor-silva@hotmail.com>
2020-03-18 16:14:01 +0100

    Fix lint problems on rgb_to_hex.rb

--- ruby/rgb_to_hex/rgb_to_hex.rb
+++ ruby/rgb_to_hex/rgb_to_hex.rb
@@ -1,21 +1,19 @@
-def rgb(r, g, b)
-  return '' if r.nil? or g.nil? or b.nil?
+# frozen_string_literal: true
 
-  r = 0 if r < 0
-  g = 0 if g < 0
-  b = 0 if b < 0
+def rgb(red, green, blue)
+  return '' if red.nil? || green.nil? || blue.nil?
 
-  r = 255 if r > 255
-  g = 255 if g > 255
-  b = 255 if b > 255
+  red = convert(red)
+  green = convert(green)
+  blue = convert(blue)
 
-  rh = r.to_s(16).upcase
-  gh = g.to_s(16).upcase
-  bh = b.to_s(16).upcase
+  "#{red}#{green}#{blue}"
+end
 
-  rh += rh if rh.length == 1
-  gh += gh if gh.length == 1
-  bh += bh if bh.length == 1
+def convert(value)
+  value = 0 if value.negative?
 
-  "#{rh}#{gh}#{bh}"
+  value = 255 if value > 255
+
+  value.to_s(16).rjust(2, '0').upcase
 end
